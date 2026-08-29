c170b6385b290971eb170329ae1d7355f39aef89
obmsuya <obmsuya@gmail.com>
2026-08-29 22:50:05 +0300

    change home page to include wapangaji kiganjani

--- main/views.py
+++ main/views.py
@@ -17,4 +17,17 @@ def services(request):
 
 
 def contact(request):
+
+    if request.method == "POST":
+
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        phone = request.POST.get("phone")
+        message = request.POST.get("message")
+
+        print("NAME:", name)
+        print("EMAIL:", email)
+        print("PHONE:", phone)
+        print("MESSAGE:", message)
+
     return render(request, "main/contact.html")
